3a771a04a4156fd0ed8753ca955b7f07476b29a7
solera2 <rushtre1@gmail.com>
2021-12-19 14:52:03 -0500

    created class/constructor for employee and pre-imported all required modules into index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+//node modules 
+const fs = require('fs');
+const inquirer = require('inquirer');
+
+//js team profiles 
+const Engineer = require('lib/Engineer.js');
+const Intern = require('lib/Intern.js');
+const Manager = require('lib/Manager.js');
+
+//html page
+const Html = require('src/generateHTML.js');
+
+
