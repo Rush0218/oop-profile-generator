b058858dfc12cafea4b01b4f1c9084bba6fdd107
solera2 <rushtre1@gmail.com>
2021-12-19 16:21:50 -0500

    added a stylesheet

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -63,21 +63,21 @@ const generatePage = function (employees) {
         <html lang="en">
         <head>
             <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <title>Team Profile</title>
-            <link href="https://fonts.googleapis.com/css?family=Public+Sans:300i,300,500&display=swap" rel="stylesheet">
-            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
             <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
             <link rel="stylesheet" href="style.css">
         </head>
         <body>
             <header>
                 <nav class="navbar" id="navbar">
-                    <span class="navbar-brand mb-0 h1 w-100 text-center" id="navbar-text">Team Profile</span>
+                    <span class="navbar-title mb-0 h1 w-100 text-center" id="navbar-text">Team Profile</span>
                 </nav>
             </header>
             <main>
-                <div class="container">
+                <div class="teamcontainer">
                     <div class="row justify-content-center" id="team-cards">
                         <!--Team Cards-->
                         ${employees}
@@ -86,30 +86,14 @@ const generatePage = function (employees) {
             </main>
             
         </body>
-        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
     </html>
 `
 };
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//function to generate html page depending on input
 generateHTML = (data) => {
 
     //create an array for each team container
@@ -121,7 +105,26 @@ generateHTML = (data) => {
 
         //team member functions
         if (role === 'Engineer') {
-            const engineer = generate
+            const engineer = generateEngineer(employee);
+
+            dataArr.push(engineer);
+        }
+
+        if (role === 'Intern') {
+            const intern = generateIntern(employee);
+
+            dataArr.push(intern);
+        }
+
+        if (role === 'Manager') {
+            const manager = generateManager(employee);
+
+            dataArr.push(manager);
         }
     }
-}
+    const team = dataArr.join('');
+    const generateTeam = generatePage(team);
+    return generateTeam;
+};
+
+module.exports = generateHTML; 
